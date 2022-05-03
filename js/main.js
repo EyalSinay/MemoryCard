b8681b0cc36d3f618cfab6cca5d35fe3534f2cfd
amir gilboa <amirg76@gmail.com>
2022-05-03 13:00:48 +0300

    update css files

--- js/main.js
+++ js/main.js
@@ -481,3 +481,36 @@ function generateRandomColor() {
 function setRandomColorToBody() {
   document.body.style.background = `linear-gradient(135deg, ${generateRandomColor()} 0%, ${generateRandomColor()} 50%, ${generateRandomColor()} 100%)`;
 }
+
+const startButton = document.querySelector(".exitBtn");
+const setTime = document.querySelector(".timer");
+listenToStartButton();
+
+function listenToStartButton() {
+  startButton.addEventListener("click", function () {
+    const sec = 0;
+    function setTime() {
+        ++totalSeconds;
+        secondsLabel.innerHTML = pad(totalSeconds % 60);
+        minutesLabel.innerHTML = pad(parseInt(totalSeconds / 60));
+      }
+    
+}
+
+function pad(val) {
+    return val > 9 ? val : "0" + val;
+  }
+  setInterval(function () {
+    document.getElementById("seconds").innerHTML = pad(++sec % 60);
+    document.getElementById("minutes").innerHTML = pad(
+      parseInt(sec / 60, 10)
+    );
+  }, 1000);
+});
+// function setUpdateTimer() {
+//   const clock = new Date();
+//   const minutes = clock.getMinutes();
+//   console.log(minutes);
+//   const seconds = clock.getSeconds();
+//   setTime.innerText = `${clock.minutes}:${clock.seconds}`;
+// }
