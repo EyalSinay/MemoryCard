dd7d559026fcccc2d81c47488fb6dcf420514db2
amir gilboa <amirg76@gmail.com>
2022-05-03 23:29:13 +0300

    commit new laout function

--- js/game.js
+++ js/game.js
@@ -1,40 +1,60 @@
-
 const statusObj = {
-    open: false,
-    currentCard: null,
-    userChoice: null,
-    waiting: false,
-    score: 0,
+  open: false,
+  currentCard: null,
+  userChoice: null,
+  waiting: false,
+  score: 1,
+};
 
-}
-
-function handleCardClick(card){
-    // OPENING CARD ONE  //
-    card.setAttribute("data-open", true);
-    if (!statusObj.open){
-        statusObj.open = true;
+function handleCardClick(card) {
+  // OPENING CARD ONE  //
+  card.setAttribute("data-open", true);
+  if (!statusObj.open) {
+    statusObj.open = true;
     statusObj.currentCard = card.getAttribute("data-pairNum");
     // console.log(statusObj);
-} else {
-   
+  } else {
     checkMatch(card);
-}
-
+  }
 }
 // CARD TWO CHECK MATCH//
-function checkMatch(card){
+function checkMatch(card) {
+  if (card.getAttribute("data-pairNum") === statusObj.currentCard) {
+    statusObj.score++;
+    const allOpen = document.querySelectorAll("[data-open='true']");
+    //! ADD DELAY HERE //
+    allOpen.forEach((e) => {
+      e.style.visibility = "hidden";
+    });
+    statusObj.open = false;
 
-    if (card.getAttribute("data-pairNum") === statusObj.currentCard){
-        statusObj.score++;
-       const allOpen = document.querySelectorAll("[data-open='true']");
-//! ADD DELAY HERE //
-       allOpen.forEach((e)=>{
-           e.style.visibility = "hidden";
-        })
-        statusObj.open = false;
-       
     //     console.log(statusObj);
-    } else{
-        
-    }
+  } else {
+  }
+}
+
+// ADD POINT TO SCORE //
+export function addPointToScore() {
+  const scoreStatus = document.querySelector(".winningCount");
+  let currentScore = scoreStatus.innerText;
+  statusObj.score++;
+  scoreStatus.innerText = statusObj.score;
+  AnimateScore(scoreStatus);
+}
+
+// ANIMATE SCORE AND TURN OVER
+function AnimateScore(StatusBoardEL) {
+  StatusBoardEL.setAttribute("data-addPoint", "true");
+  setTimeout(() => {
+    StatusBoardEL.setAttribute("data-addPoint", "false");
+  }, 4000);
+}
+
+//  ADD POINT TO TURN OVER
+export function addPointToTurnOver() {
+  const turnOverCountStatus = document.querySelector(".turnOverCount");
+  let currentTurnOverCount = turnOverCountStatus.innerText;
+  statusObj.score++;
+  turnOverCountStatus.innerText = statusObj.score;
+  AnimateScore(turnOverCountStatus);
 }
--- js/start-page.js
+++ js/start-page.js
@@ -1,107 +1,129 @@
-import { objCards } from './obj_cards.js';
-import { shuffleArray } from './tools.js';
+import { objCards } from "./obj_cards.js";
+import { shuffleArray } from "./tools.js";
+import { startTimer } from "./tools.js";
+//only for check
+import { addPointToScore } from "./game.js";
+import { addPointToTurnOver } from "./game.js";
 
 function getUserChoices(e) {
-    const inputsFormCollection = e.target.querySelectorAll('#start-page-form input');
-    const inputsFormElementsTruth = [...inputsFormCollection].filter(option => option.checked === true);
-    const userChoicesObj = {};
-    inputsFormElementsTruth.forEach(element => {
-        let val = element.value;
-        if (typeof val === 'string') {
-            val = parseInt(val, 10);
-        }
-        userChoicesObj[element.name] = val;
-    });
-    return userChoicesObj;
+  const inputsFormCollection = e.target.querySelectorAll(
+    "#start-page-form input"
+  );
+  const inputsFormElementsTruth = [...inputsFormCollection].filter(
+    (option) => option.checked === true
+  );
+  const userChoicesObj = {};
+  inputsFormElementsTruth.forEach((element) => {
+    let val = element.value;
+    if (typeof val === "string") {
+      val = parseInt(val, 10);
+    }
+    userChoicesObj[element.name] = val;
+  });
+  return userChoicesObj;
 }
 
 function createMatrix(numOfCards) {
-    let rows;
-    let columns;
-    if (numOfCards === 12) {
-        rows = 3;
-        columns = 4;
-    } else if (numOfCards === 18) {
-        rows = 3;
-        columns = 6;
-    } else if (numOfCards === 24) {
-        rows = 4;
-        columns = 6;
-    }
+  let rows;
+  let columns;
+  if (numOfCards === 12) {
+    rows = 3;
+    columns = 4;
+  } else if (numOfCards === 18) {
+    rows = 3;
+    columns = 6;
+  } else if (numOfCards === 24) {
+    rows = 4;
+    columns = 6;
+  }
 
-    const emptyCardsArr = [];
-    const columnsArr = new Array(columns);
-    for (let i = 0; i < rows; i++) {
-        emptyCardsArr.push([...columnsArr]);
-    }
+  const emptyCardsArr = [];
+  const columnsArr = new Array(columns);
+  for (let i = 0; i < rows; i++) {
+    emptyCardsArr.push([...columnsArr]);
+  }
 
-    // set whe greed property:
-    document.documentElement.style.setProperty('--columns', columns);
-    document.documentElement.style.setProperty('--rows', rows);
+  // set whe greed property:
+  document.documentElement.style.setProperty("--columns", columns);
+  document.documentElement.style.setProperty("--rows", rows);
 
-    return emptyCardsArr;
+  return emptyCardsArr;
 }
-
 
 function getRandomArrMatrix(arr, numOfCards) {
-    const idOfCards = objCards.getIdOfCards();
+  const idOfCards = objCards.getIdOfCards();
 
-    shuffleArray(idOfCards);
+  shuffleArray(idOfCards);
 
-    idOfCards.splice(0, idOfCards.length - numOfCards/2);
-    idOfCards.push(...idOfCards);
+  idOfCards.splice(0, idOfCards.length - numOfCards / 2);
+  idOfCards.push(...idOfCards);
 
-    shuffleArray(idOfCards);
+  shuffleArray(idOfCards);
 
-    for (let i = 0; i < arr.length; i++) {
-        for (let j = 0; j < arr[i].length; j++) {
-            arr[i][j] = idOfCards[0];
-            idOfCards.shift();
-        }
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr[i].length; j++) {
+      arr[i][j] = idOfCards[0];
+      idOfCards.shift();
     }
-    return arr;
+  }
+  return arr;
 }
-
 
 //* creates HTML elements based on received grid and chosen image set.
-function createElements (grid, imageSet) {
-    const board = document.querySelector("#gameBoard");
-    for (let i = 0; i < grid.length; i++){
-        let rand = Math.floor(Math.random()*objCards.length);
-        const card = document.createElement("img");
-        card.setAttribute("data-pairNum", grid[i]);
-        card.setAttribute("src", objCards.cards[`card${grid[i]}`][`image${imageSet}`]);
-        card.setAttribute("data-open", "false");
-        card.addEventListener('click', ()=>handleCardClick(card)); //!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-        board.appendChild(card);
-    }
+function createElements(grid, imageSet) {
+  const board = document.querySelector("#gameBoard");
+  for (let i = 0; i < grid.length; i++) {
+    let rand = Math.floor(Math.random() * objCards.length);
+    const card = document.createElement("img");
+    card.setAttribute("data-pairNum", grid[i]);
+    card.setAttribute(
+      "src",
+      objCards.cards[`card${grid[i]}`][`image${imageSet}`]
+    );
+    card.setAttribute("data-open", "false");
+    card.addEventListener("click", () => handleCardClick(card)); //!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    board.appendChild(card);
+  }
 }
 
-
 export function submitEventListener(e) {
-    e.preventDefault();
+  e.preventDefault();
+  // START THE TIME WHEN GAME START
+  setTimeout(startTimer, 1000);
+  // test
+  addPointToScore();
+  addPointToTurnOver();
+  //test end
 
-    const userChoicesObj = getUserChoices(e);
-    console.log(userChoicesObj);
+  const userChoicesObj = getUserChoices(e);
+  console.log(userChoicesObj);
 
-    const arrEmpty = createMatrix(userChoicesObj["num-of-cards"]);
-    console.log(arrEmpty);
+  const arrEmpty = createMatrix(userChoicesObj["num-of-cards"]);
+  console.log(arrEmpty);
 
-    const matrixCardsNumbers = getRandomArrMatrix(arrEmpty, userChoicesObj['num-of-cards']);
-    console.log(matrixCardsNumbers);
-    
+  const matrixCardsNumbers = getRandomArrMatrix(
+    arrEmpty,
+    userChoicesObj["num-of-cards"]
+  );
+  console.log(matrixCardsNumbers);
 
-    //* createElements()   /////
+  //* createElements()   /////
 
-    // display-none start page with animation:
-    const startPageMainContainer = document.querySelector('.start-page-main-container');
-    startPageMainContainer.addEventListener('animationend', e => {
-        e.target.setAttribute('data-display', 'false');
-    }, { once: true });
-    startPageMainContainer.setAttribute('animation', 'rotate_scale_opacity-out');
+  // display-none start page with animation:
+  const startPageMainContainer = document.querySelector(
+    ".start-page-main-container"
+  );
+  startPageMainContainer.addEventListener(
+    "animationend",
+    (e) => {
+      e.target.setAttribute("data-display", "false");
+    },
+    { once: true }
+  );
+  startPageMainContainer.setAttribute("animation", "rotate_scale_opacity-out");
 
-    // display-grid game page with animation:
-    const gameContainer = document.querySelector(".game-container");
-    gameContainer.setAttribute("data-display", "true");
-    gameContainer.setAttribute("animation", "rotate_scale_opacity-in");
-};
+  // display-grid game page with animation:
+  const gameContainer = document.querySelector(".game-container");
+  gameContainer.setAttribute("data-display", "true");
+  gameContainer.setAttribute("animation", "rotate_scale_opacity-in");
+}
